07f82ec3fd6c3136b351886fe21264383220085b
root <root@example.com>
2018-09-21 20:44:53 +0800

    完善Application

--- ucc-rest/src/main/java/cn/cucsi/bsd/ucc/Application.java
+++ ucc-rest/src/main/java/cn/cucsi/bsd/ucc/Application.java
@@ -3,10 +3,8 @@ package cn.cucsi.bsd.ucc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan("cn.cucsi.bsd.ucc")
+@SpringBootApplication(scanBasePackages = {"cn.cucsi.bsd.ucc"})
 @MapperScan("cn.cucsi.bsd.ucc.common.mapper")
 public class Application {
     public static void main(String[] args){
