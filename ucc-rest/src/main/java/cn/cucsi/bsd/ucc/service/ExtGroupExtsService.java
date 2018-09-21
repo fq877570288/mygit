ea543c91e3e57bace1a7662671d284d13bd48852
zhaosongshan <912150317@qq.com>
2018-09-21 14:45:06 +0800

    3.26.2获取分机组、坐席员、分机号集合

--- ucc-rest/src/main/java/cn/cucsi/bsd/ucc/service/ExtGroupExtsService.java
+++ ucc-rest/src/main/java/cn/cucsi/bsd/ucc/service/ExtGroupExtsService.java
@@ -1,9 +1,13 @@
 package cn.cucsi.bsd.ucc.service;
 
 import cn.cucsi.bsd.ucc.common.beans.ExtGroupExtsCriteria;
+import cn.cucsi.bsd.ucc.common.beans.PageResultBean_New;
 import cn.cucsi.bsd.ucc.data.domain.ExtGroupExts;
 import cn.cucsi.bsd.ucc.data.domain.ExtGroupExtsPK;
+import cn.cucsi.bsd.ucc.data.domain.PbxExtGroups;
 import org.springframework.data.domain.Page;
+
+import java.util.List;
 
 /**
  * Created by Song on 2017/10/16.
@@ -13,4 +17,5 @@ public interface ExtGroupExtsService {
     ExtGroupExts findOne(ExtGroupExtsPK ExtGroupExtsPK);
     ExtGroupExts save(ExtGroupExts ExtGroupExts);
     Boolean delete(ExtGroupExtsPK ExtGroupExtsPK);
+    PageResultBean_New<List<PbxExtGroups>> queryDashboardList(String domainId);
 }
