dea3c22ada12c9b7be65045b3df89526bf7867fd
zhaosongshan <912150317@qq.com>
2018-09-28 14:00:01 +0800

    移植主页echars接口 更改移植代码中的bug,修改数据封装

--- ucc-rest/src/main/java/cn/cucsi/bsd/ucc/rest/controllers/HomeController.java
+++ ucc-rest/src/main/java/cn/cucsi/bsd/ucc/rest/controllers/HomeController.java
@@ -5,14 +5,12 @@ import cn.cucsi.bsd.ucc.data.domain.*;
 import cn.cucsi.bsd.ucc.service.*;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import jdk.nashorn.internal.scripts.JS;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.text.SimpleDateFormat;
