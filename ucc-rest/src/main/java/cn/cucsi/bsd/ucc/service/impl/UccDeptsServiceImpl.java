1c13242d449bf56f939c71add2b260fc1476eeca
zhaosongshan <912150317@qq.com>
2018-10-12 16:56:42 +0800

    增加查询部门的子部门 增加try catch

--- ucc-rest/src/main/java/cn/cucsi/bsd/ucc/service/impl/UccDeptsServiceImpl.java
+++ ucc-rest/src/main/java/cn/cucsi/bsd/ucc/service/impl/UccDeptsServiceImpl.java
@@ -1,6 +1,8 @@
 package cn.cucsi.bsd.ucc.service.impl;
 
 import cn.cucsi.bsd.ucc.common.beans.NgtDeptSearch;
+import cn.cucsi.bsd.ucc.common.beans.PageResultBean;
+import cn.cucsi.bsd.ucc.common.beans.ResultBean;
 import cn.cucsi.bsd.ucc.common.beans.UccDeptsCriteria;
 import cn.cucsi.bsd.ucc.common.mapper.UccDeptsMapper;
 import cn.cucsi.bsd.ucc.common.untils.MyUtils;
@@ -415,5 +417,44 @@ public class UccDeptsServiceImpl implements UccDeptsService {
             e.printStackTrace();
             return null;
         }
+    }
+    public void test(UccDepts rootDepts, List<UccDepts> source, List<UccDepts> all){
+        List<UccDepts> ownDepts = source.stream().filter(row-> row.getDeptPid().equals(rootDepts.getDeptId())).collect(Collectors.toList());
+        if(MyUtils.isBlank(ownDepts)){
+            return;
+        }
+        all.addAll(ownDepts);
+        for(UccDepts o: ownDepts){
+            test(o, source, all);
+        }
+
+    }
+
+
+    /**
+     * 查询用户关联的部门及其子部门
+     */
+    public List<UccDepts> queryChildrenByDepts(String domainId,List<UccDepts> list){
+        UccDeptsCriteria search = new UccDeptsCriteria();
+        search.setDomainId(domainId);
+        ResultBean<List<UccDepts>> bean = new PageResultBean(this.uccDeptsService.findAllTree(search));
+        List<UccDepts> source = bean.getData();
+        List<UccDepts> all = new ArrayList<>();
+        try{
+            for(UccDepts filter: list){
+                UccDepts rootDept = source.stream().filter(row-> row.getDeptId().equals(filter.getDeptId())).findFirst().get();
+                if(rootDept != null){
+                    all.add(rootDept);
+                    this.test(rootDept, source, all);
+                }
+            }
+            for(UccDepts uccDepts:all){
+                System.out.println(uccDepts.getDeptId()+" "+uccDepts.getDeptPid()+" "+list.size());
+            }
+        }catch (Exception e){
+            System.out.println("询用户关联的部门及其子部门失败！");
+            e.printStackTrace();
+        }
+        return all;
     }
 }
