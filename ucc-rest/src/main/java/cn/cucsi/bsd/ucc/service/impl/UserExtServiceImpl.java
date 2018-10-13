940b85e35c2c2d4d28487ac162c3400d1c695f8b
zhangxiaofei <7431673@qq.com>
2018-10-13 17:15:45 +0800

    分配分机——By Mybatis

--- ucc-rest/src/main/java/cn/cucsi/bsd/ucc/service/impl/UserExtServiceImpl.java
+++ ucc-rest/src/main/java/cn/cucsi/bsd/ucc/service/impl/UserExtServiceImpl.java
@@ -1,6 +1,7 @@
 package cn.cucsi.bsd.ucc.service.impl;
 
 import cn.cucsi.bsd.ucc.common.beans.UserExtCriteria;
+import cn.cucsi.bsd.ucc.common.mapper.UserExtMapper;
 import cn.cucsi.bsd.ucc.data.domain.UserExt;
 import cn.cucsi.bsd.ucc.data.repo.UserExtRepository;
 import cn.cucsi.bsd.ucc.data.specs.UserExtSpecs;
@@ -12,6 +13,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
  * Created by mk on 2017/10/16.
  */
@@ -19,6 +22,8 @@ import org.springframework.stereotype.Service;
 public class UserExtServiceImpl  implements UserExtService{
     @Autowired
     private UserExtRepository userExtRepository;
+    @Autowired
+    private UserExtMapper userExtMapper;
     @Override
     public Page<UserExt> findAll(UserExtCriteria criteria) {
         Sort sort = new Sort(Sort.Direction.DESC, "userId");
@@ -31,6 +36,19 @@ public class UserExtServiceImpl  implements UserExtService{
         return userExtRepository.findOne(userId);
     }
 
+    @Override
+    public  List<UserExt> findByUserId(UserExt userExt) {
+        return userExtMapper.findByUserId(userExt);
+    }
+    @Override
+    public int del(UserExt userExt) {
+        return userExtMapper.deleteByPrimaryKey(userExt);
+    }
+
+    @Override
+    public int insert(UserExt userExt) {
+        return userExtMapper.insert(userExt);
+    }
     @Override
     public UserExt save(UserExt userExt) {
         return userExtRepository.save(userExt);
