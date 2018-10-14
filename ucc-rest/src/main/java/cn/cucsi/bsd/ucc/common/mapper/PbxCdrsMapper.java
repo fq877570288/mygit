2495013a1334d22e3043003193967fc6ecb9e4d4
tianyuwei <tianyuwei@tianyuweideMacBook-Air.local>
2018-10-14 11:29:23 +0800

    质检员界面 取数接口完善

--- ucc-rest/src/main/java/cn/cucsi/bsd/ucc/common/mapper/PbxCdrsMapper.java
+++ ucc-rest/src/main/java/cn/cucsi/bsd/ucc/common/mapper/PbxCdrsMapper.java
@@ -1,5 +1,6 @@
 package cn.cucsi.bsd.ucc.common.mapper;
 
+import cn.cucsi.bsd.ucc.common.beans.CountBean;
 import cn.cucsi.bsd.ucc.common.beans.PageResultBean_New;
 import cn.cucsi.bsd.ucc.common.beans.PbxCdrsCriteria;
 import cn.cucsi.bsd.ucc.common.beans.PbxCdrsForAPPCriteria;
@@ -18,5 +19,9 @@ public interface PbxCdrsMapper {
     //根据条件查询通话记录列表（web端查询通话记录） by zhaolei 2018-09-04
     List<PbxCdrs> selectByPrimary(PbxCdrsCriteria pbxCdrsCriteria);
     //根据条件查询通话记录列表（web端查询通话记录） by zhoalie 2018-09-04
-    List<PbxCdrs> selectByPrimaryExcel(PbxCdrsCriteria pbxCdrsCriteria);  
+    List<PbxCdrs> selectByPrimaryExcel(PbxCdrsCriteria pbxCdrsCriteria);
+
+    CountBean vivCount();
+    CountBean queueCount();
+
 }
--- ucc-rest/src/main/java/cn/cucsi/bsd/ucc/rest/controllers/DashboardController.java
+++ ucc-rest/src/main/java/cn/cucsi/bsd/ucc/rest/controllers/DashboardController.java
@@ -1,12 +1,16 @@
 package cn.cucsi.bsd.ucc.rest.controllers;
 
 import cn.cucsi.bsd.ucc.common.JSONView;
+import cn.cucsi.bsd.ucc.common.beans.CountBean;
 import cn.cucsi.bsd.ucc.common.beans.PageResultBean_New;
 import cn.cucsi.bsd.ucc.common.beans.ResultBean;
 import cn.cucsi.bsd.ucc.common.beans.UccUserCriteria;
+import cn.cucsi.bsd.ucc.common.mapper.PbxCdrsMapper;
+import cn.cucsi.bsd.ucc.data.domain.PbxCdrs;
 import cn.cucsi.bsd.ucc.data.domain.PbxExtGroups;
 import cn.cucsi.bsd.ucc.data.domain.UccUsers;
 import cn.cucsi.bsd.ucc.service.ExtGroupExtsService;
+import cn.cucsi.bsd.ucc.service.PbxCdrsService;
 import cn.cucsi.bsd.ucc.service.UccUserService;
 import com.fasterxml.jackson.annotation.JsonView;
 import com.google.gson.JsonObject;
@@ -35,6 +39,9 @@ public class DashboardController {
     @Autowired
     private UccUserService uccUserService;
 
+    @Autowired
+    private PbxCdrsMapper pbxCdrsMapper;
+
     @ApiOperation(value = "查询机组、坐席员、分机号集合", notes = "查询机组、坐席员、分机号集合")
     @RequestMapping(value = "/list", method =  RequestMethod.GET)
     public PageResultBean_New<List<PbxExtGroups>> list(String domainId){
@@ -47,7 +54,7 @@ public class DashboardController {
         }
     }
     @ApiOperation(value = "坐席总数、今日人工、今日IVR、示忙、示闲", notes = "坐席总数、今日人工、今日IVR、示忙、示闲")
-    @RequestMapping(value = "/allNumbers", method = RequestMethod.GET)
+    @RequestMapping(value = "/allNumbers", method = RequestMethod.GET,produces="application/json;charset=utf-8")
     public String allNumbers(String domainId){
         JsonObject j = new JsonObject();
         try {
@@ -59,11 +66,16 @@ public class DashboardController {
                 uccUserCriteria.setDomainId(domainId);
                 uccUsersList = uccUserService.querySeater(uccUserCriteria);
             }
+
+            //获取今日IVR
+            CountBean ivrCount = pbxCdrsMapper.vivCount();
+            CountBean queueCount = pbxCdrsMapper.queueCount();
+
             j.addProperty("return_msg","success");
             j.addProperty("return_code","success");
             j.addProperty("seatTotal",uccUsersList.size());//坐席总数
-            j.addProperty("manTotal",666);//今日人工
-            j.addProperty("ivrTotal",666);//今日IVR
+            j.addProperty("manTotal",queueCount.getCount());//今日人工
+            j.addProperty("ivrTotal",ivrCount.getCount());//今日IVR
             j.addProperty("callingQueueTotal",0);//人工通话中
             j.addProperty("callingIvrTotal",0);//IVR通话中
             return j.toString();
