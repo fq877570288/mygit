26ad7063afca26fe49e85b08253b1c83fae6f4cd
tianyuwei <tianyuwei@tianyuweideMacBook-Air.local>
2018-10-16 13:57:08 +0800

    租户管理完善

--- ucc-rest/src/main/java/cn/cucsi/bsd/ucc/rest/controllers/UccDomainController.java
+++ ucc-rest/src/main/java/cn/cucsi/bsd/ucc/rest/controllers/UccDomainController.java
@@ -78,13 +78,13 @@ public class UccDomainController {
         ResultBean<Object> resultBean = new ResultBean();
         if(loginUser != null && "uccAdmin".equals(loginUser.getUserId())){
 //            uccDomain.setCreatedTime(new Date());
+            return uccDomainService.createDomin(uccDomain);
         }
         else{
             resultBean.setCode(1);
             resultBean.setMsg("该用户没有此操作权限");
+            return resultBean;
         }
-
-        return uccDomainService.createDomin(uccDomain);
     }
 
     @ApiOperation(value = "修改UccDomain", notes = "修改UccDomain")
