364150b8144fba9c6e9b6839d9296677dd193250
zhaosongshan <912150317@qq.com>
2018-10-17 16:24:10 +0800

    改bug

--- ucc-rest/src/main/java/cn/cucsi/bsd/ucc/data/domain/UccCustomers.java
+++ ucc-rest/src/main/java/cn/cucsi/bsd/ucc/data/domain/UccCustomers.java
@@ -28,6 +28,7 @@ public class UccCustomers {
     private Integer msgResource;
     private String memo;
     private String idcard;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date birthday;
     private String qq;
     private String email;
@@ -290,6 +291,7 @@ public class UccCustomers {
 
     @Basic
     @Column(name = "birthday", nullable = true)
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     public Date getBirthday() {
         return birthday;
     }
