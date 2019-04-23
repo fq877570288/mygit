9542bf71390cb388551d80afef0cb11bef1680dc
fuqi <877570288@qq.com>
2019-04-23 15:16:53 +0800

    第二次提交

--- ucc-rest/src/main/java/cn/cucsi/bsd/ucc/data/repo/TeamUsersRepository.java
+++
@@ -1,8 +0,0 @@
-package cn.cucsi.bsd.ucc.data.repo;
-
-import cn.cucsi.bsd.ucc.data.domain.TeamUsers;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface TeamUsersRepository  extends PagingAndSortingRepository<TeamUsers,String>,JpaSpecificationExecutor {
-}
